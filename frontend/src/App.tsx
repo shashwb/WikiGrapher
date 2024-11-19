b48f4766b087ca26138bd1d97663a9b6a1d57b44
Seth <sethbalodi@gmail.com>
2024-11-19 13:13:17 -0500

    setting up tailwind css for the first time

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,16 +7,37 @@ import GraphView from "./pages/GraphView";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 
+/** components */
+import Navbar from "./components/Navbar";
+
+/** styling using tailwind */
+
 const App: React.FC = () => {
+  const style = {
+    red: {
+      backgroundColor: "red",
+    },
+  };
+
   const layout = (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/graph" element={<GraphView />} />
-      </Routes>
-    </Router>
+    <>
+      {/* <div className="bg-blue-50 min-h-screen"> */}
+      <div className="bg-blue-50 min-h-screen">
+        <div className={"background-color: red"}>testing linkin park</div>
+        <Router>
+          <Navbar />
+          <main className="container mx-auto p-4">
+            <div>ya</div>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/graph" element={<GraphView />} />
+            </Routes>
+          </main>
+        </Router>
+      </div>
+    </>
   );
 
   return layout;
