6519a01067e55af11febbc4f338a98beed6f9c65
Seth <sethbalodi@gmail.com>
2024-11-20 23:37:40 -0500

    removed 'isolated: modules' option in typescript config, finally have the auth0 library working'

--- frontend/src/components/AuthProvider.tsx
+++ frontend/src/components/AuthProvider.tsx
@@ -1,2 +1,19 @@
 import React from "react";
 import { Auth0Provider } from "@auth0/auth0-react";
+
+/** Provide authentication to all the child components within the app */
+const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  return (
+    <Auth0Provider
+      domain="YOUR_AUTH0_DOMAIN"
+      clientId="YOUR_AUTH0_CLIENT_ID"
+      redirectUri={window.location.origin}
+    >
+      {children}
+    </Auth0Provider>
+  );
+};
+
+export default AuthProvider;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
 /** tailwind */
 import "./index.css";
+/** Auth */
+// import AuthProvider from "./components/AuthProvider";
+import AuthProvider from "./components/AuthProvider";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
