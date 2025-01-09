2693b056699f0591c5aad6e9c5546d464de79a18
Seth <sethbalodi@gmail.com>
2025-01-09 14:54:07 -0500

    fixed ts d3 location issue, increased my understanding of how CRA compiles typescript and how to reset the TS server during development

--- frontend/src/pages/GraphView.tsx
+++ frontend/src/pages/GraphView.tsx
@@ -1,9 +1,18 @@
 /** page for displaying the graph view  */
-import React from "react";
+import React, { useEffect } from "react";
+
+/** adding d3 */
+import * as d3 from "d3";
+
 import { useAuth0 } from "@auth0/auth0-react";
 
 const GraphView: React.FC = () => {
   const { isAuthenticated } = useAuth0();
+
+  useEffect(() => {
+    // const svg = sum([1, 2, 3]);
+    // console.log("svg", svg);
+  }, []); // run once, on load
 
   if (!isAuthenticated) {
     return <div>Log In to view your Graph</div>;
